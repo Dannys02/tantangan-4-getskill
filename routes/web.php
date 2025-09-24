51c50691f11e4e52ff69494344d3a8db17a1f0c0
Dannys02 <webdannys@gmail.com>
2025-09-24 09:02:46 +0700

    Selesai

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Saya bantuan Ai buat nambahin ini dan install controller studentnya
+use App\Http\Controllers\StudentController;
+
 Route::get('/', function () {
     return view('layouts.app');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/form', function () {
+    return view('students.form');
+});
+
+Route::get('/student', function () {
+    return view('students.student');
 });
